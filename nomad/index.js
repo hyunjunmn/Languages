122c7bb0f96abf6a23fbc5fc84a4fcf5f96aad5e
hyunjunmn <gus6751@gmail.com>
2020-01-06 20:45:50 +0900

    Day1_extra

--- nomad/index.js
+++ nomad/index.js
@@ -1,3 +1,36 @@
-let a =221;
-let b = a -10;
-console.log(b);
+/*const a = "asdf";
+let b =221;
+console.log(a,b);
+alert('Hi Everyone');
+// 기본적 변수선언은 const(변수 변경 불가)로 let은 변경이 가능한 변수형 
+*/
+/*
+Boolean 형
+const a = false;
+//const what = true;
+*/
+/*Float 형
+const a = 55.1;
+*/
+/*
+Array 형
+const daysOfWeek = ["Mon","Tue","Wed","Thr","Fri","Sat","Sun"];
+
+console.log(daysOfWeek[1]);
+*/
+
+// Object 형
+const info ={
+    name:"Jun",
+    age:23,
+    gender:"Male",
+    favMovies:["OLD BOY","LOTR","KIMCHI"],
+    favFood: [
+        {name:"Kimchi",
+            fatty:false},
+
+        {name:"Cheese",
+            fatty:true}]
+}
+
+console.log(info);
